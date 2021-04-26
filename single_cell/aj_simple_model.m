e24a4dae322f33e631944f543c2bb9f46e95fa74
Alice <ajef8473@uni.sydney.edu.au>
2021-04-26 21:36:17 +1000

    Added Alice's model version (modified from Hugo's), removed CT state, added trap states, modified constants to include trap states.

---
+++ single_cell/aj_simple_model.m
@@ -0,0 +1,48 @@
+function dydt = aj_simple_model(G0,ks)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% dydt = [dExcitions; dChargeTransport; dFreeCharge]
+
+%% Initialise inputs
+if nargin <= 2
+    k1 = 1e10;
+    
+    kd1 = 1e7;
+    
+    kr = 1e7;
+    
+    kt = 1e5;
+    kdt = 1e5;
+    T = 1e22; 
+    
+else
+    k1 = ks(1); %exciton dissociation rate
+    kd1 = ks(2); %non-radiative exciton decay
+    kr = ks(3); %recombination rate
+    
+    kt = ks(4); %trapping rate
+    kdt = ks(5); %detrapping rate
+    T = ks(6); %concentration of traps in material
+    
+    G0 = ks(7); %generation rate
+end
+
+
+%% take derivatives
+
+    function dydt = derivs(t,y)
+        Ex = y(1);
+        TS = y(2);
+        FC = y(3);       
+        
+        dEx = G0 - kd1 * Ex -k1 * Ex;
+        dTS = kt*(T-TS)*FC - kdt*(TS^2 + TS*FC);
+        dFC = k1 * Ex - kr * FC^2 - kt*Ex + kdt*Ex;
+        
+        dydt = [dEx; dTS; dFC];
+    end
+
+dydt = @derivs;
+
+end
+
--- single_cell/constants.m
+++ single_cell/constants.m
@@ -1,13 +1,13 @@
-%%
-%Generation rate calculations: take an incident intensity in W/m^2 at a
-%given wavelength, and output a generation rate of electron/hole pairs. 
+%% Generation rate calculations: 
+% take an incident intensity in W/m^2 at a given wavelength, and output a 
+% generation rate of electron/hole pairs. 
 
-%global constants:
+%% constant constants:
 h = 6.626e-34; %Planck's constant, m^2 kg / s
-c = physconst("Lightspeed"); %speed of light, m/s
+c = physconst("Lightspeed"); 
 
-%user inputted:
-incidentIntensity = 1000; %W/m^2
+%% user inputted:
+I = 1000; %W/m^2, incident intensity
 incidentWavelength = 500e-9; %m
 absorbance = 1.5e6; %photons absorbed per metre of film thickness, #photons / m
 filmThickness = 100e-9; %m
@@ -16,26 +16,32 @@ filmThickness = 100e-9; %m
 photonEnergy = (h * c) ./ incidentWavelength; %energy of the given incident photons, J
 photonFluxDensity = incidentIntensity ./ photonEnergy; %photon flux density, #photons.m^-2.s^-1
 
-GenerationRate = photonFluxDensity .* absorbance; %photons.m^-3.s^-1
-
-%% kr2: bimolecular recombination rate: 10^-3 to 10^-5
-kr2 = 10e-3 %m^-3.s^-1
+G0 = photonFluxDensity .* absorbance; %photons.m^-3.s^-1
 
 %% k1: how rapidly do excitons dissociate
 %assume a temperature-activated Arrhenius eqn for a binding energy
-k1 = exp
+k1 = 10^12 %s^-1 (STRANK)
 
 %% kd1: decay rate one. How quickly do excitons decay (non-radiatively)
-kd1 = 1e-6 .* 1e6 %%cm^3/s, up to 250e-6
+kd1 = 1e7 %%s^-1 (STRANK) (or 1 - 250 e6, HERZ)
 
+%% kr: bimolecular recombination rate: 10^-3 to 10^-5
+kr = 10e-3 %m^-3.s^-1 (HERZ)(SAJID)
 
+%% Trapping, detrapping rates and trap concentration (STRANKS 2014)
+kt = 2e-4 %m^3.s^-1
+kdt = 8e-6 %m^3.s^-1
+T = 2.5e22 %m^-3 (STRANK) (or 10e22 - 10e23, HERZ)
 
+%% list of constants
+k1 = ks(1); %exciton dissociation rate
+kd1 = ks(2); %non-radiative exciton decay
+kr = ks(3); %recombination rate
 
+kt = ks(4); %trapping rate
+kdt = ks(5); %detrapping rate
+T = ks(6); %concentration of traps in material
 
-
-
-
-
-
+G0 = ks(7); %generation rate
 
 
